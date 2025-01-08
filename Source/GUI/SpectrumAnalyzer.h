09fa8cdc664b66ed435791a49ca7557294f977c3
Diego Ignacio Ortega Martínez <diegortegaimi@gmail.com>
2025-01-08 16:45:26 -0600

    Fixed negative infinity line being visible in spectrum analyzer
    refactor how FFT paths are drawn

--- Source/GUI/SpectrumAnalyzer.h
+++ Source/GUI/SpectrumAnalyzer.h
@@ -17,34 +17,36 @@ juce::AudioProcessorParameter::Listener,
 juce::Timer
 {
     SpectrumAnalyzer(SimpleMBCompAudioProcessor&);
-        ~SpectrumAnalyzer();
-        
-        void parameterValueChanged (int parameterIndex, float newValue) override;
-        void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { }
-        
-        void timerCallback() override;
-        
-        void paint(juce::Graphics& g) override;
-        void resized() override;
-        
-        void toggleAnalysisEnablement(bool enabled)
-        {
-            shouldShowFFTAnalysis = enabled;
-        }
-    private:
-        SimpleMBCompAudioProcessor& audioProcessor;
-        bool shouldShowFFTAnalysis = true;
-        juce::Atomic<bool> parametersChanged { false };
-        
-        void drawBackgroundGrid(juce::Graphics& g, juce::Rectangle<int> bounds);
-        void drawTextLabels(juce::Graphics& g, juce::Rectangle<int> bounds);
-        
-        std::vector<float> getFrequencies();
-        std::vector<float> getGains();
-        std::vector<float> getXs(const std::vector<float>& freqs, float left, float width);
-        juce::Rectangle<int> getRenderArea(juce::Rectangle<int> bounds);
-        
-        juce::Rectangle<int> getAnalysisArea(juce::Rectangle<int> bounds);
-        
-        PathProducer leftPathProducer, rightPathProducer;
+    ~SpectrumAnalyzer();
+    
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+    void parameterGestureChanged (int parameterIndex, bool gestureIsStarting) override { }
+    
+    void timerCallback() override;
+    
+    void paint(juce::Graphics& g) override;
+    void resized() override;
+    
+    void toggleAnalysisEnablement(bool enabled)
+    {
+        shouldShowFFTAnalysis = enabled;
+    }
+private:
+    SimpleMBCompAudioProcessor& audioProcessor;
+    bool shouldShowFFTAnalysis = true;
+    juce::Atomic<bool> parametersChanged { false };
+    
+    void drawBackgroundGrid(juce::Graphics& g, juce::Rectangle<int> bounds);
+    void drawTextLabels(juce::Graphics& g, juce::Rectangle<int> bounds);
+    
+    std::vector<float> getFrequencies();
+    std::vector<float> getGains();
+    std::vector<float> getXs(const std::vector<float>& freqs, float left, float width);
+    juce::Rectangle<int> getRenderArea(juce::Rectangle<int> bounds);
+    
+    juce::Rectangle<int> getAnalysisArea(juce::Rectangle<int> bounds);
+    
+    PathProducer leftPathProducer, rightPathProducer;
+    
+    void drawFFTAnalysis(juce::Graphics& g, juce::Rectangle<int> bounds);
 };
